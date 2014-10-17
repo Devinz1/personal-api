9cf36d7f1c68a81f378565f842061a37cc1a2311
devinz1 <devinzohner@gmail.com>
2014-10-16 20:45:20 -0600

    server up and running

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var express = require('express');
 
 var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(bodyParser.json());
+
+	var port = 8080;
+    
+    app.get('*', function(req, res){
+    	res.end('Hello World');
+
+    });
+  app.listen(8080)
